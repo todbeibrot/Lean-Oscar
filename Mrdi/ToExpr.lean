4e97e4daa59e64ccb066435b9fe13969473fe5e7
Cedric Holle <cedricholle0@gmail.com>
2024-04-10 01:04:59 +0200

    ToExpr Equiv.Perm Fin n

--- Mrdi/ToExpr.lean
+++ Mrdi/ToExpr.lean
@@ -2,42 +2,16 @@ import Mathlib
 
 /- This file should contain diverse `ToExpr` instances -/
 
-
 open Qq
+open Lean hiding Rat mkRat
 
+section DebuggingTools
 
-#check Lean.ToExpr
-#check Rat.mk'
-
-#reduce Rat.mk' (2 : ℤ) (3 : ℕ)
-
-#check Rat.mul
-#check Rat.ofInt
-
-def ofInt (num : Int) : Rat := { num, reduced := Nat.coprime_one_right (Int.natAbs num) }
-
-#check mkRat
-
-/- Rat.mul { num := Int.ofNat 2, den := 1, den_nz := Rat.ofInt.proof_1, reduced := ⋯ }
-  (Rat.inv { num := Int.ofNat 3, den := 1, den_nz := Rat.ofInt.proof_1, reduced := ⋯ }) -/
-
-instance : Lean.ToExpr ℚ where
-  toTypeExpr := Lean.mkConst ``Rat
-  toExpr q := match q with
-    | ⟨num, den, _, _⟩ =>
-        --let numE : Q(ℤ) := Lean.toExpr num
-        --let denE : Q(ℕ) := Lean.toExpr den
-        --let qE : Q(ℚ) := q(Rat.mk' $numE $denE)
-        Lean.mkApp2 (Lean.mkConst ``mkRat) (Lean.toExpr num) (Lean.toExpr den)
-
-#reduce Lean.toExpr (2 : ℚ)
-
-open Lean Meta Lean.Elab Command Term
-
+open Meta Lean.Elab Command Term
 -- for debugging
 elab "#testToExpr " val:term : command =>
   liftTermElabM do
-    let valE ← Lean.Elab.Term.elabTerm val none
+    let valE ← Term.elabTerm val none
     let t : Q(Type) ← inferType valE
     let valE : Q($t) := valE
     let _ ← synthInstanceQ q(ToExpr $t)
@@ -60,14 +34,84 @@ elab "#testToExpr " val:term : command =>
         else
           logInfo s!"is not definitionally equal: \n\n e: {e} \n\n valE: {valE}"
 
+-- for debugging
 elab "#myreduce " val:term : command =>
   liftTermElabM do
-    let valE ← Lean.Elab.Term.elabTerm val none
+    let valE ← Term.elabTerm val none
     let valE ← reduce valE
     logInfo valE
 
-#myreduce ((2 : ℤ) / (3 : ℕ) : ℚ )
-#eval ((2 : ℤ) / (3 : ℕ) : ℚ )
+end DebuggingTools
 
+section Rat
 
-#testToExpr ((2 : ℤ) / (3 : ℕ) : ℚ )
+instance : ToExpr ℚ where
+  toTypeExpr := mkConst ``Rat
+  toExpr q := match q with
+    | ⟨num, den, _, _⟩ =>
+        mkApp2 (mkConst ``mkRat) (toExpr num) (toExpr den)
+
+end Rat
+
+-- section Cycle
+
+-- open List
+
+-- #check Quotient.rep
+
+-- instance [ToExpr α] : ToExpr (Cycle α) where
+--   toTypeExpr := mkApp (mkConst ``Cycle) (toTypeExpr α)
+--   toExpr s := sorry
+
+-- def test1 [DecidableEq α] [Fintype α] [LinearOrder α] (s : Cycle α) : List α := by
+--   let x : Quotient (IsRotated.setoid α) := s
+--   --let y := Quotient.rep x
+
+--   sorry
+
+-- end Cycle
+
+section Permutations
+
+open Equiv Equiv.Perm
+
+-- -- TODO should we panic instead of using default?
+-- def Cycle.formPerm' [DecidableEq α] (s : Cycle α) : Perm α :=
+--   if h : s.Nodup then Cycle.formPerm s h else default
+
+-- -- TODO should we panic instead of using default?
+-- instance [Fintype α] [LinearOrder α] [DecidableEq α] [ToExpr α] : ToExpr (Equiv.Perm α) where
+--   toTypeExpr := mkApp (mkConst ``Equiv.Perm) (toTypeExpr α)
+--   toExpr p :=
+--     let ⟨l, h_lprod, h_isCycle, h_pairwiseDisjoint⟩ := cycleFactors p
+--     let l2 := l.map (fun p => if h : p ∈ l then toCycle p (h_isCycle p h) else default)
+--     let l3 := l2.map Cycle.formPerm'
+--     let new_p := l3.prod
+--     sorry
+
+-- def test (p : Perm (Fin (n + 1))) : List (List (Fin (n + 1))) :=
+--   Id.run <| do
+--     let ⟨cycles, _⟩ := cycleFactors p
+--     let mut l : List (List (Fin (n + 1))) := []
+--     for cycle in cycles do
+--       for i in List.range (n + 1) do
+--         if cycle.toFun i ≠ i then
+--           let aux : List (Fin (n + 1)) := cycle.toList i
+--           l := aux :: l
+--           break
+--     return l
+
+instance : ToExpr (Equiv.Perm (Fin (n + 1))) where
+  toTypeExpr := mkApp (mkConst ``Equiv.Perm) (toTypeExpr (Fin n))
+  toExpr p := Id.run <| do
+    let ⟨cycles, _⟩ := cycleFactors p
+    let mut l : List (List (Fin (n + 1))) := []
+    for cycle in cycles do
+      for i in List.range (n + 1) do
+        if cycle.toFun i ≠ i then
+          let aux : List (Fin (n + 1)) := cycle.toList i
+          l := aux :: l
+          break
+    return q((List.map List.formPerm $l).prod)
+
+end Permutations
