642d5f779474c3337f331e1d1d346a2ccd25cd25
todbeibrot <holle.cedric@gmx.de>
2024-04-26 21:49:24 +0200

    RS julia test

--- Mrdi/test.jl
+++ Mrdi/test.jl
@@ -15,10 +15,23 @@ vector = Vector([1, 2, 3])
 matrix_inv = inv(matrix)
 F = free_group(2)
 (f1, f2) = gens(F)
-quot = quo(F, [f1^2, f2^2, comm(f1, f2)])[1]
+(quot, proj) = quo(F, [f1^2, f2^2, comm(f1, f2)])
 rels = relators(quot)
 
-file = "fp_group_rels"
-save("mrdi-files/$file.mrdi", rels)
-x = load("mrdi-files/$file.mrdi")
-println(x)
+# file = "fp_group_rels"
+# save("mrdi-files/$file.mrdi", rels)
+# x = load("mrdi-files/$file.mrdi")
+# println(x)
+
+# rw system
+println("start loading")
+GAP.Packages.load("kbmag", install=true)
+println(GAP.Packages.locate_package("kbmag"))
+f = free_group(2)
+(a, b) = gens(f)
+rels = [comm(a, b) / a, comm(b, a) / b]
+g = quo(f, rels)[1]
+g2 = GAP.GapObj(g)
+s = GAP.Globals.KBMAGRewritingSystem(g2)
+GAP.Globals.SetInfoLevel(GAP.Globals.InfoRWS, 0)
+GAP.Globals.MakeConfluent(s)
